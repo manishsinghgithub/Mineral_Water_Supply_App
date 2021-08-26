b330f70afcefd41de5eb58303bdb7dd37a18c0de
Manish Singh <manishsinghonline2@gmail.com>
2021-08-26 20:02:17 +0530

    form added

---
+++ lib/screens/form.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DataForm extends StatelessWidget {
+  String? _name, _adrees, _contact;
+  final String? money, count, qua;
+  DataForm({this.money, this.count, this.qua});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,31 +5,66 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue[300],
-        elevation: 0,
-        leading: Icon(Icons.home),
-        title: Text("Home"),
-        actions: [
-          FlatButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => Profile()));
-              },
-              child: Icon(
-                Icons.person,
-                color: Colors.white,
-              )),
-          FlatButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Icon(
-                Icons.logout,
-                color: Colors.white,
-              )),
-        ],
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.blue[300],
+          elevation: 0,
+          leading: Icon(Icons.home),
+          title: Text("Home"),
+          actions: [
+            FlatButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Profile()));
+                },
+                child: Icon(
+                  Icons.person,
+                  color: Colors.white,
+                )),
+            FlatButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Icon(
+                  Icons.logout,
+                  color: Colors.white,
+                )),
+          ],
+        ),
+        body: Padding(
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+          child: ListView(
+            children: [
+              Container(
+                height: 300,
+                color: Colors.green,
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Container(
+                height: 300,
+                color: Colors.yellow,
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Container(
+                height: 300,
+                color: Colors.red,
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              FlatButton(
+                onPressed: () {},
+                child: Text("Done and Continue"),
+                color: Colors.blue[300],
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
